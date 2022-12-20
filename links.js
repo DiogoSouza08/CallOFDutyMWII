456cdf63a5c795d3bcc860d3e05d211d6d17efe7
Diogo Souza <115508662+DiogoSouza08@users.noreply.github.com>
2022-12-20 19:38:51 -0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,8 @@
+function trailer(){
+    window.open("https://www.youtube.com/watch?v=r72GP1PIZa0", '_blank');
+  }
+
+function comprar(){
+    window.open("https://www.callofduty.com/br/pt/modernwarfare2/buy", '_blank');
+  }
+ 
