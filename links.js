5bd4006df4564fef54ab58d57665d4a938606047
Diogo Souza <115508662+DiogoSouza08@users.noreply.github.com>
2022-12-23 15:37:43 -0300

    Update links.js

--- links.js
+++ links.js
@@ -6,3 +6,37 @@ function comprar(){
     window.open("https://www.callofduty.com/br/pt/modernwarfare2/buy", '_blank');
   }
  
+  var video = document.getElementById("mp4");
+var spinner = document.getElementById("spinner");
+var delayMillis = 4000;
+var spinnerIsHere = 1;
+video.volume = 0;
+
+var playVid = setTimeout(function() {
+  if(spinnerIsHere == 1) {
+    // Delete element DOM
+    // spinner.parentNode.removeChild(spinner);
+    spinner.style.visibility = "hidden";
+    spinnerIsHere = 0;
+  }
+  video.play();
+}, delayMillis);
+
+video.addEventListener("click", function( event ) {
+  if(video.paused) {
+    if(spinnerIsHere == 1) {
+      // Delete element DOM
+      // spinner.parentNode.removeChild(spinner);
+      spinner.style.visibility = "hidden";
+      spinnerIsHere = 0;
+    }
+    clearTimeout(playVid);
+    video.play();
+  } else {
+    video.pause();
+    if(spinnerIsHere == 0) {
+      spinner.style.visibility = "visible";
+      spinnerIsHere = 1;
+    }
+  }
+}, false);
